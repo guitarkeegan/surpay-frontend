a8386f60c9716e99a181da63b2a489f79eec2e41
guitarkeegan <keegananglim@gmail.com>
2022-11-10 13:48:13 -0800

    add user and company model

--- components/CreateSurveyForm.js
+++ components/CreateSurveyForm.js
@@ -6,16 +6,20 @@ export default function CreateSurveyForm() {
     const [questions, setQuestion] = useState("")
     const [surveyTitle, setSurveyTitle] = useState("")
 
-    
-
+    const addNewQuestion = () => {
+        
+    }
 
     return (
         <div>
         <h1>{surveyTitle ? surveyTitle : "Survey Title?"}</h1>
         <form>
             <label className="mr-2">Survey Title</label>
-            <input onChange={(e) => setSurveyTitle(e.target.value)} className="border-2 p-2 rounded-lg border-slate-500 mr-2" name="survey-title"></input>
-            <button className="border-2 p-2 rounded-lg hover:bg-slate-500" type="button">Start Adding Questions</button>
+            <input autoComplete="off" onChange={(e) => setSurveyTitle(e.target.value)} className="border-2 p-2 rounded-lg border-slate-500 mr-2" name="survey-title"></input>
+            <button
+             className="border-2 p-2 rounded-lg hover:bg-slate-500" type="button"
+            //  onSubmit={}
+             >Add Question</button>
         </form>
         </div>
     )
---
+++ db/models/User.js
@@ -0,0 +1,50 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true,
+      validate: {
+        isEmail: true,
+      }
+    },
+  }, {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  });
+  return User;
+};
