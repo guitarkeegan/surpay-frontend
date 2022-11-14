c5debcc2e2dd004e2818e209a9c81a50447b5bbf
guitarkeegan <keegananglim@gmail.com>
2022-11-13 23:38:21 -0800

    working on login

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,20 @@
-import { MoralisProvider } from "react-moralis";
-import { NotificationProvider } from "web3uikit";
-import '../styles/globals.css'
-import Navbar from "../components/Navbar";
+import { MoralisProvider } from "react-moralis"
+import { NotificationProvider } from "web3uikit"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Layout from "../components/Layout";
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <MoralisProvider initializeOnMount={false}>
-      <NotificationProvider>
-        <Navbar>
-          <Component {...pageProps} />
-        </Navbar>
-      </NotificationProvider>
-    </MoralisProvider>
-  )
+
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+    return (
+            <MoralisProvider initializeOnMount={false}>
+                <NotificationProvider>
+                    <Layout>
+                        <Component {...pageProps} />
+                    </Layout>
+                </NotificationProvider>
+            </MoralisProvider>
+    )
 }
 
 export default MyApp
---
+++ pages/api/login/distributer/index.js
@@ -0,0 +1,39 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+import { ironOptions } from "../../../../lib/iron-session-config";
+import { Company } from "../../../../db/models/"
+
+export default withIronSessionApiRoute(loginRoute, ironOptions);
+
+async function loginRoute(req, res) {
+  // get user from database
+  try {
+    const companyData = await Company.findOne({ where: { email: req.body.email } });
+
+
+    if (!companyData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email, please try again" });
+      return;
+    }
+
+    const validPassword = companyData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect password, please try again" });
+      return;
+    }
+} catch (e) {
+    console.log("hit catch in login api route")
+    console.error(e);
+}
+
+  req.session.user = {
+    id: 230,
+    admin: true,
+  };
+  await req.session.save();
+  res.send({ ok: true });
+}
