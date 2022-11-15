88adf2769c92e9cbba103368b2b9662ad4d37e6d
guitarkeegan <keegananglim@gmail.com>
2022-11-14 16:28:50 -0800

    add survey folder

---
+++ pages/api/survey/[sid].js
@@ -0,0 +1,18 @@
+import {Survey, Question, Answer, User} from "../../../db/models"
+
+export default async function handler(req, res){
+
+    const method = req.method
+
+    switch (method) {
+        case "GET":
+            // 
+            break;
+        case "POST":
+
+            break;
+        default:
+            break;
+    }
+
+}
