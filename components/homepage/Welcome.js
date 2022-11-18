0f819c57433746c335160b50b07758480b4a8434
Alan Abed <alanabed@Alans-MacBook-Air.local>
2022-11-17 18:20:52 -0800

    hompage almost there nav bar needs work

--- components/homepage/Welcome.js
+++ components/homepage/Welcome.js
@@ -3,20 +3,19 @@ import surveyTaker from "../../public/assets/img/surveyTakerImage.jpg"
 import ethereumImg from "../../public/assets/img/ethereumImg.jpg"
 import surpayLogoImg from "../../public/assets/img/surpayLogo.png"
 import Button from 'react-bootstrap/Button'
-// import Container from 'react-bootstrap/Container';
-// import Row from 'react-bootstrap/Row';
-// import Col from 'react-bootstrap/Col';
 import styles from "../../styles/Welcome.module.css"
 
 
 export default function Welcome() {
     return (
         <sectiion>
+
             <div className={styles.welcomeCon}>
                 <div className={styles.welcomeSection}>
                     <h1 className={styles.welcomeTitle}>Welcome to Surpay</h1>
                     <h2 className={styles.welcomeStatement}>Take a survey and get paid in crypto</h2>
                 </div>
+
                 <section className={styles.topCon}>
                     <div className={styles.firstSectionLeft}>
                         <Image 
@@ -26,14 +25,13 @@ export default function Welcome() {
                         </Image>
                     </div>
                     <div className={styles.firstSectionRight}>
-                        <h2 className={styles.firstParagraph}>lorem ipsum lorem ipsum lore heello jhshsy ksjsus ksushsjs sjsususn snsjsush nsjs sj s</h2>
+                        <h2 className={styles.firstParagraph}>It is as easy as signing up, choosing a survey, completing it, and gettimng paid in crypto. </h2>
                     </div>
-                    
                 </section>
 
                 <section className={styles.secondCon}>
                     <div className={styles.secondSectionLeft}>
-                    <h3 className={styles.secondParagraph}>lorem ipsum hello there how  jshshs jsjhs sj sk sk sj sks js s hsjshisj hsbsjsn</h3>
+                    <h3 className={styles.secondParagraph}>Connecting companies straight to the user while maintaing users privacy. </h3>
                     </div>
                     <div className={styles.secondSectionRight}>
                         <Image 
@@ -48,30 +46,25 @@ export default function Welcome() {
                 </section>
 
             </div>
-                <footer className={styles.footerCon}>
-                    <div className={styles.footerLogo}>
-                        <Image 
-                        src={surpayLogoImg}
-                        alt={"Surpay Logo"}
-                        className={styles.surpayLogoImg}>
-                        </Image>
-                        <h2 className={styles.followUsFooter}>Follow Us</h2>
-                        <h3 className={styles.legalFooter}>Legal</h3>
+            <footer className={styles.footerCon}>
+                <div className={styles.footerLogo}>
+                     <Image 
+                    src={surpayLogoImg}
+                    alt={"Surpay Logo"}
+                    className={styles.surpayLogoImg}>
+                    </Image>
                     </div>
-                    <div>
+                <div>
+                    <h2 className={styles.topFooterChainlink}>Powered By Chainlink</h2>
+                 </div>
+                <div>
                     <h2 className={styles.middleFooterPolygon}>Powered By Polygon</h2>
-                    </div>
-                    <div>
+                </div>
+                <div>
                     <h2 className={styles.bottomFooterText}>2022 Surpay. All Rights Reserved</h2>
-                    </div>
-                </footer>
-
+                </div>
+            </footer>
         </sectiion>
     );
-  } 
-                    // <div className={styles.secondSectionLeft}>
-                    // <h3 className={styles.secondParagraph}>lorem ipsum hello there how </h3>
-                    // </div>
-                    // <div className={styles.firstSectionRight}>
-                    //     <h2 className={styles.firstParagraph}>lorem ipsum lorem ipsum lore</h2>
-                    // </div>
+}
+    
