8dd3fa5a2e1e22dc13b5b9f618decba7acc74776
guitarkeegan <keegananglim@gmail.com>
2023-01-28 08:02:36 -0800

    working on captcha

--- components/modals/CaptchaModal.js
+++ components/modals/CaptchaModal.js
@@ -1,90 +1,65 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import axios from "axios";
-import Loader from '../loader/Loader';
-
+import React, { useState } from "react"
+import Button from "react-bootstrap/Button"
+import Modal from "react-bootstrap/Modal"
+import axios from "axios"
+import Loader from "../loader/Loader"
 
 // import captcha from "/assets/img/recaptchaExample.png"
-import {useRouter} from "next/router"
-import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
-import { ModalBody } from 'react-bootstrap';
+import { useRouter } from "next/router"
+import { useGoogleReCaptcha } from "react-google-recaptcha-v3"
+import { ModalBody } from "react-bootstrap"
 
-export default function MockCaptchaModal({user, selectedSurveyId}) {
-  const [show, setShow] = useState(false);
-  const [isBtnDisabled, setBtnDisabled] = useState(false);
-  const [response, setResponse] = useState(null);
-  const { executeRecaptcha } = useGoogleReCaptcha();
-
-  const router = useRouter()
-
-  const handleSubmit = async () => {
-
-    setBtnDisabled(true);
-
-    if (!executeRecaptcha) {
-      return;
-    }
-
-    try {
-      const token = await executeRecaptcha();
-      if (!token) {
-        setResponse({ message: "Failed to Send!!!", status: "Failed" });
-        return;
-      }
-
-      const result = await axios.post("/api/captcha", {
-        token
-      });
-      console.log(result);
-
-      if (result.data) {
-        setResponse({
-          message: result.data.message,
-          status: result.data.status,
-        });
-      }
-      setBtnDisabled(false);
-
-      router.push(`/survey/take/${selectedSurveyId}`)
-
-    } catch (error) {
-      console.log(error);
-      setResponse({ message: "Failed to Send!!!", status: "Failed" });
-      setBtnDisabled(false);
-    }
-  
-
-
-
+export default function CaptchaCheck({ user, selectedSurveyId }) {
     
+    const [isBtnDisabled, setBtnDisabled] = useState(false)
+    const [response, setResponse] = useState(null)
+    const { executeRecaptcha } = useGoogleReCaptcha()
 
-  }
+    const router = useRouter()
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+    const handleSubmit = async () => {
+        setBtnDisabled(true)
 
-  return (
-    <>
-      <Button variant="primary" onClick={handleShow}>
-        Take me to the survey
-      </Button>
+        if (!executeRecaptcha) {
+            return
+        }
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Captcha</Modal.Title>
-        </Modal.Header>
-        <Modal.ModalBody>
-          {response.status === "Failed" && response.message}
-        </Modal.ModalBody>
-        <Modal.Footer>
-          <Button disabled={isBtnDisabled} variant="primary" onClick={handleSubmit}>
-          <span>Submit{isBtnDisabled && "ting"}</span>
-          {isBtnDisabled && <Loader />}
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
+        try {
+            const token = await executeRecaptcha()
+            if (!token) {
+                setResponse({ message: "Failed to Send!!!", status: "Failed" })
+                return
+            }
+
+            const result = await axios.post("/api/captcha", {
+                token,
+            })
+            console.log(result)
+
+            if (result.data) {
+                setResponse({
+                    message: result.data.message,
+                    status: result.data.status,
+                })
+            }
+            setBtnDisabled(false)
+
+            router.push(`/survey/take/${selectedSurveyId}`)
+        } catch (error) {
+            console.log(error)
+            setResponse({ message: "Failed to Send!!!", status: "Failed" })
+            setBtnDisabled(false)
+        }
+    }
+
+    return (
+        <>
+            <p>{response?.status === "Failed" && response?.message}</p>
+
+            <Button disabled={isBtnDisabled} variant="primary" onClick={handleSubmit}>
+                <span>Submit{isBtnDisabled && "ting"}</span>
+                {isBtnDisabled && <Loader />}
+            </Button>
+        </>
+    )
 }
-
